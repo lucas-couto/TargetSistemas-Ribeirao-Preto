00427996f2d50e3a7a07e674bd009c0ba46a4316
lucas-couto <lslopes1903@hotmail.com>
2022-06-15 17:58:08 -0300

    first commit

---
+++ Ex5.js
@@ -0,0 +1,12 @@
+function inverterString() {
+    let palavra = 'programacao'
+    let palavraInvertida = ''
+    let fim = palavra.length-1
+    while (fim != 0) {
+        palavraInvertida += palavra[fim]
+        fim--
+    }
+    return palavraInvertida
+}
+
+console.log(inverterString())
